fb54885cffb310e73a99488b54f4d4809b9148fa
Mugilvannan P <mugilvannanpichai02@gmail.com>
2024-11-18 14:35:46 +0530

    deploy springboot

---
+++ src/test/java/com/example/samdeploy/SamdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
